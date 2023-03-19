6f5baceb95bf9b18560ce58cd6daf4a26c5defbe
tian <istiancz@gmail.com>
2023-03-20 00:35:13 +0800

    feat: db options

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
-import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { topicRouter } from './routers/topic'
+import { createTRPCRouter } from "~/server/api/trpc"
+import { exampleRouter } from "~/server/api/routers/example"
 
 /**
  * This is the primary router for your server.
@@ -8,7 +9,8 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
-});
+  topic: topicRouter
+})
 
 // export type definition of API
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
